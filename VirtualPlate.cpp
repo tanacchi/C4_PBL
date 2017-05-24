f4d4f8b964578c8e8cf2b5a7f20f8d93c571e6fc
tanacchi <q111026d@mail.kyutech.jp>
2017-05-24 12:50:03 +0900

    Rename virtual plate class

--- VirtualPlate.cpp
+++ VirtualPlate.cpp
@@ -22,14 +22,14 @@ enum class Stone {
 };
 
 
-class Plate {
+class VirtualPlate {
   std::vector <std::vector <Stone> > plate;
   Stone active_stone;
 public:
-  Plate();
-  Plate(const Plate& plate);
-  const Plate& operator=(const Plate& src);
-  ~Plate();
+  VirtualPlate();
+  VirtualPlate(const VirtualPlate& plate);
+  const VirtualPlate& operator=(const VirtualPlate& src);
+  ~VirtualPlate();
   Task init();
   Task show() const;
   char convert_stone_to_char(Stone stone) const;
@@ -42,37 +42,37 @@ public:
   int get_length(int x, int y, int dx, int dy) const;
 };
 
-Plate::Plate()
+VirtualPlate::VirtualPlate()
   : plate {std::vector<std::vector<Stone> > (PLATE_HEIGHT, std::vector<Stone> (PLATE_WIDTH))},
     active_stone {Stone::Red}
 {
   init();
 }
 
-Plate::Plate(const Plate& src)
+VirtualPlate::VirtualPlate(const VirtualPlate& src)
   : plate {src.plate},
     active_stone {src.active_stone}
 {
 }
 
-const Plate& Plate::operator=(const Plate& src) {
+const VirtualPlate& VirtualPlate::operator=(const VirtualPlate& src) {
   plate = src.plate;
   active_stone = src.active_stone;
   return *this;
 }  
 
-Plate::~Plate()
+VirtualPlate::~VirtualPlate()
 {
 }
 
-Task Plate::init() {
+Task VirtualPlate::init() {
   for (int i = 0; i < PLATE_HEIGHT; i++)
     for (int j = 0; j < PLATE_WIDTH; j++)
       plate[i][j] = Stone::Space;
   return Task::Show;
 }
 
-Task Plate::show() const {
+Task VirtualPlate::show() const {
   std::cout << "-----------------" << std::endl;
   std::cout << "| ";
   for (int i = 0; i < PLATE_WIDTH; i++) std::cout << i << ' ';
@@ -87,29 +87,29 @@ Task Plate::show() const {
   return Task::Set;
 }
 
-char Plate::convert_stone_to_char(Stone stone) const {
+char VirtualPlate::convert_stone_to_char(Stone stone) const {
   return
     (stone == Stone::Red)  ? 'O':
     (stone == Stone::Blue) ? 'X':
     ' ';
 }
 
-bool Plate::insert(int input_x) {
+bool VirtualPlate::insert(int input_x) {
   if (plate[0][input_x] != Stone::Space) return false;
   for (int y = 0; y < PLATE_HEIGHT; y++) {
     if (!can_drop(input_x, y)) { plate[y][input_x] = active_stone; return true; };
   }
 }
 
-bool Plate::can_drop(int x, int y) const {
+bool VirtualPlate::can_drop(int x, int y) const {
   return (y < PLATE_HEIGHT-1) && (plate[y+1][x] == Stone::Space);
 }
 
-void Plate::switch_active_stone() {
+void VirtualPlate::switch_active_stone() {
   active_stone = (active_stone == Stone::Red) ? Stone::Blue : Stone::Red;
 }
 
-bool Plate::can_continue() const {
+bool VirtualPlate::can_continue() const {
   int count = 0;
   for (int i = 0; i < PLATE_HEIGHT; i++)
     for (int j = 0; j < PLATE_WIDTH; j++)
@@ -117,11 +117,11 @@ bool Plate::can_continue() const {
   return (count > 0);
 }
 
-bool Plate::is_inside_plate(int x, int y) const {
+bool VirtualPlate::is_inside_plate(int x, int y) const {
   return (0 <= x && x < PLATE_WIDTH) && (0 <= y && y < PLATE_HEIGHT);
 }
 
-bool Plate::is_game_finish() const {
+bool VirtualPlate::is_game_finish() const {
   const std::vector<int> dx = { 1, 1, 1, 0 };
   const std::vector<int> dy = {-1, 0, 1, 1 };
   for (int i = 0; i < 4; i++) 
@@ -133,7 +133,7 @@ bool Plate::is_game_finish() const {
   return false;
 }
 
-int Plate::get_length(int x, int y, int dx, int dy) const {
+int VirtualPlate::get_length(int x, int y, int dx, int dy) const {
   int i;
   for (i = 1; is_inside_plate(x+i*dx, y+i*dy); i++)
     if (plate[y+i*dy][x+i*dx] == active_stone) continue;
@@ -145,7 +145,7 @@ int main() {
 
   srand((unsigned)time(NULL));
   Task task {Task::Init};  
-  Plate plate;
+  VirtualPlate plate;
 
   plate.show();
 

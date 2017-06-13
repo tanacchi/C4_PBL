d053e999ec5e4e63e3641a60a07c8f892afba5e6
tanacchi <q111026d@mail.kyutech.jp>
2017-06-13 11:46:14 +0900

    Add class of BodyController

--- include/BodyController.h
+++ include/BodyController.h
@@ -1,6 +1,16 @@
 #ifndef BODY_CONTROLLER_H_
 #define BODY_CONTROLLER_H_
 
+#include <EVShield.h>
+#include <EVs_EV3Touch.h>
 
+#define LANE_NUM 6
+
+class BodyController {
+  Evshield evshield;
+  Evs_EV3Touch touch_sensor[LANE_NUM];
+ public:
+  BodyController();  
+}; 
 
 #endif // BODY_CONTROLLER_H_
--- src/BodyController.cpp
+++ src/BodyController.cpp
@@ -1,1 +1,8 @@
 #include "../include/BodyController.h"
+
+BodyController::BodyController()
+  : evshield(0x34, 0x36)
+{
+  evshield.init(SH_HardwareI2C);
+  for (int i = 0; i < LANE_NUM; i++) touch[i].init(&evshield, ); // <- what port??
+}

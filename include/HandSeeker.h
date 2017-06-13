ae18c7cf85574f3b2c55d597c1d0363709bafb17
tanacchi <q111026d@mail.kyutech.jp>
2017-06-13 09:09:19 +0900

    Remove HandList class

--- include/HandSeeker.h
+++ include/HandSeeker.h
@@ -18,15 +18,15 @@ class HandList {
 
 class HandSeeker {
   VirtualPlate myplate_;
+  int* hand_list_;
   HandSeeker* sub_;
-  HandList* hand_list_;
   const unsigned int max_depth_; // <- byte ??
   unsigned int mydepth_;    // <- byte ??
   int branch_;
  public:
   HandSeeker();
   HandSeeker(const HandSeeker& src);
-  ~HandSeeker() = default;
+  ~HandSeeker();
   int operator()(const VirtualPlate& game_plate);
   const HandSeeker& operator=(const HandSeeker& src);
   void set_myplate(const VirtualPlate& src);
--- src/HandSeeker.cpp
+++ src/HandSeeker.cpp
@@ -1,35 +1,9 @@
 #include "../include/HandSeeker.h"
 
-HandList::HandList()
-  : position_ { 0 },
-    score_ { 0.0 }
-{
-}
-
-// HandList::~HandList() {
-//   std::cout << "Bye!" << std::endl;
-// }
-
-void HandList::set_position(int x) {
-  position_ = x;
-}
-
-void HandList::set_score(double s) {
-  score_ = s;
-}
-
-double HandList::get_score() const {
-  return score_;
-}
-
-int HandList::get_position() const {
-  return position_;
-}
-
 HandSeeker::HandSeeker()
   : myplate_(),
     sub_ { 0 },
-    max_depth_ { 3 },
+    max_depth_ { 1 },
     mydepth_ { 0 },
     branch_ { 0 }
 {
@@ -44,8 +18,9 @@ HandSeeker::HandSeeker(const HandSeeker& src)
 {
 }
 
-// HandSeeker::~HandSeeker() {
-// }
+HandSeeker::~HandSeeker() {
+  if (hand_list_) delete[] hand_list_;
+}
 
 int HandSeeker::operator()(const VirtualPlate& game_plate) {
   myplate_ = game_plate;
@@ -62,23 +37,29 @@ const HandSeeker& HandSeeker::operator=(const HandSeeker& src) {
 void HandSeeker::record_list() {             // REFACTOR: どう考えても二度手間
   for (int i = 0; i < PLATE_WIDTH; i++)
     if (myplate_.is_valid_hand(i)) branch_++;
-  hand_list_ = new HandList[branch_]; 
+  std::cout << "AHI !! " << std::flush;
+  hand_list_ = new int[branch_];
+  std::cout << "AHI !! " << std::endl;
   for (int i = 0; i < PLATE_WIDTH; i++)
-    if (myplate_.is_valid_hand(i)) hand_list_[i].set_position(i); // [] のオーバーロードでうまいこと行くかもしれない
+    if (myplate_.is_valid_hand(i)) hand_list_[i] = i; // [] のオーバーロードでうまいこと行くかもしれない
 }
 
-int n;
+int n = 0;
 
 double HandSeeker::set_list_score() {
   double score;
   record_list();
   if (!(branch_ && max_depth_-mydepth_ > 0)) return evaluate_hand();
-  for (int i = 0; i < branch_ && n++ < 10000; i++) {
+  for (int i = 0; i < branch_; i++) {
+    std::cout << "Hello " << ++n << ' ' << std::flush;
     sub_ = new HandSeeker(*this);
-    sub_->myplate_.insert(hand_list_[i].get_position());
+    sub_->mydepth_++;
+    sub_->myplate_.insert(hand_list_[i]);
     sub_->myplate_.switch_active_stone();
+    std::cout << "How are you ? " << n << ' ' << std::flush;
     score += sub_->set_list_score();
     delete sub_;
+    std::cout << "Bye " << n << ' ' << std::endl;
   }
   return score;
 }
@@ -97,5 +78,6 @@ int HandSeeker::evaluate_hand() {
         score_buff = myplate_.get_length(x, y, dx[i], dy[i]);
         if (score_buff > max) max = score_buff;
       }
+  std::cout << "sub_ = " << sub_ << std::endl;
   return max;
 }

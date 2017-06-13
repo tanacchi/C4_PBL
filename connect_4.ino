10c4c6f32c7debdc57926c313596ebcf43394273
tanacchi <q111026d@mail.kyutech.jp>
2017-06-13 11:00:53 +0900

    Add arduino file

---
+++ connect_4.ino
@@ -0,0 +1,28 @@
+#include <Wire.h>
+#include <EVShield.h>
+#include <EVs_EV3Touch.h>
+
+EVShield evshield(0x34, 0x36);
+EVs_EV3Touch touch;
+
+void setup() {
+  evshield.init( SH_HardwareI2C );
+  evshield.bank_a.motorReset();
+
+  touch.init( &evshield, SH_BAS1);
+  setMode ????
+  touch.resetBumpCount();
+  //  evshield.waitForButtonPress(BTN_GO);
+}
+
+void loop() {
+  if (touch.getBumpCount() % 2) {
+    evshield.bank_a.motorRunDegrees(SH_Motor_1,
+                                    SH_Direction_Reverse,
+                                    10,
+                                    30,
+                                    SH_Completion_Dont_Wait,
+                                    SH_Next_Action_Brake);
+    delay(30);
+  }
+}

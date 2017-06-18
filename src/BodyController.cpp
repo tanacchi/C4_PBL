f5fad1c3fd5ad57e5ff56d333ad7405c8662438d
tanacchi <q111026d@mail.kyutech.jp>
2017-06-18 19:40:18 +0900

    Add sample of EV3 programming

--- src/BodyController.cpp
+++ src/BodyController.cpp
@@ -4,5 +4,5 @@ BodyController::BodyController()
   : evshield(0x34, 0x36)
 {
   evshield.init(SH_HardwareI2C);
-  for (int i = 0; i < LANE_NUM; i++) touch[i].init(&evshield, ); // <- what port??
+  for (int i = 0; i < LANE_NUM; i++) touch[i].init(&evshield, i);
 }

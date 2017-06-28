abd0e8109f04d0126285f6790a77eeddeff73f60
tanacchi <q111026d@mail.kyutech.jp>
2017-06-28 20:34:53 +0900

    Finish flame of HandSeeker

--- src/HandSeeker.cpp
+++ src/HandSeeker.cpp
@@ -1,26 +1,9 @@
 #include "../include/HandSeeker.hpp"
-
-HandList::HandList()
-  : is_active_ { false },
-    score_ { 0.0 }
-{
-}
-void HandList::activate() {
-  is_active_ = true;
-}
-bool HandList::is_active() const {
-  return is_active_;
-}
-void HandList::set_score(float score) {
-  score_ = score;
-}
-float HandList::get_score() const {
-  return score_;
-}
+#include <iostream>
 
 HandSeeker::HandSeeker()
   : myplate_(),
-    hand_list_(),
+    hand_list_{0, 0, 0, 0, 0, 0},
     sub_ {0},
     mystone_{Stone::Space},
     max_depth_ {3},
@@ -29,7 +12,7 @@ HandSeeker::HandSeeker()
 }
 HandSeeker::HandSeeker(const HandSeeker& src)
   : myplate_ {src.myplate_},
-    hand_list_(),
+    hand_list_{0, 0, 0, 0, 0, 0},
     sub_ {0},
     mystone_ {src.mystone_},
     max_depth_ {src.max_depth_},
@@ -39,23 +22,24 @@ HandSeeker::HandSeeker(const HandSeeker& src)
 int HandSeeker::operator()(VirtualPlate game_plate) {
   myplate_ = game_plate;
   mystone_ = myplate_.get_active_stone();
+  get_list_score();
+  for (int i = 0; i < PLATE_WIDTH; i++)
+    std::cout << "score [" << i << "] = " << hand_list_[i] << std::endl;;
   return 0;
 }
 float HandSeeker::get_list_score() {
   if ((max_depth_ - mydepth_) < 0) return evaluate_plate();
-  for (int i = 0; i < PLATE_WIDTH; i++)
-    if (myplate_.is_valid_hand(i)) hand_list_[i].activate();
   for (int i = 0; i < PLATE_WIDTH; i++) {
-    if (!hand_list_[i].is_active()) continue;
+    if (!myplate_.is_valid_hand(i)) continue;
     sub_ = new HandSeeker(*this);
     sub_->mydepth_++;
     sub_->myplate_.insert(i);
     sub_->myplate_.switch_active_stone();
-    hand_list_[i].set_score(sub_->get_list_score());
+    hand_list_[i] = sub_->get_list_score();
     delete sub_;
   }
   float sum = 0;
-  for (int i = 0; i < PLATE_WIDTH; i++) sum += hand_list_[i].get_score();
+  for (int i = 0; i < PLATE_WIDTH; i++) sum += hand_list_[i];
   return sum;
 }
 

c03196bb00e9a3fdeabd98d7cac2bded29c2b79c
tanacchi <q111026d@mail.kyutech.jp>
2017-07-29 12:36:46 +0900

    Modify for test mode

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
-g++ --std=c++14 -c src/VirtualPlate.cpp src/console_play.cpp src/HandSeeker.cpp
+g++ --std=c++14 -c src/VirtualPlate.cpp test.cpp src/HandSeeker.cpp
 g++ --std=c++14 *.o 
 rm *.o
--- test.cpp
+++ test.cpp
@@ -5,33 +5,30 @@
 int main()
 {
 
-  std::cout << sizeof(VirtualPlate) + sizeof(HandSeeker)*5 << std::endl;
+  VirtualPlate plate;
+  HandSeeker* seeker;
+  int select_x;
+  int turn = 0;
 
-  // VirtualPlate plate;
-  // HandSeeker* seeker;
-  // int select_x;
-  // int turn = 0;
-
-  // plate.show();
+  plate.show();
   
-  // while (plate.can_continue()) {
-  //   turn++;
-  //   do {
-  //     if (turn%2){
-  //       std::cin >> select_x;
-  //     }
-  //     else {
-  //       seeker = new HandSeeker(5);
-  //       select_x = seeker->get_conclusion(plate);
-  //       delete seeker;
-  //     }
-  //   } while (!plate.is_valid_hand(select_x));
-  //   plate.insert(select_x);
-  //   plate.show();
-  //   if (plate.is_game_finish()) { std::cout << "FINISH!!" << std::endl; return 0;}
-  //   plate.switch_active_stone();
-
-  // }
+  while (plate.can_continue()) {
+    turn++;
+    do {
+      if (turn%2){
+        std::cin >> select_x;
+      }
+      else {
+        seeker = new HandSeeker(5);
+        select_x = seeker->get_conclusion(plate);
+        delete seeker;
+      }
+    } while (!plate.is_valid_hand(select_x));
+    plate.insert(select_x);
+    plate.show();
+    if (plate.is_game_finish()) { std::cout << "FINISH!!" << std::endl; return 0;}
+    plate.switch_active_stone();
+  }
   
   return 0;
 }
